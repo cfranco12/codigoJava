8a0da93d8f141934f72f89630ed80e020e79b566
Hitss <Hitss@HDFLAP>
2017-08-15 17:42:24 -0500

    Codigo fuente

--- src/depuravias/DepuraVias.java
+++ src/depuravias/DepuraVias.java
@@ -16,6 +16,7 @@ public class DepuraVias {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Modificación para github");
     }
     
 }
